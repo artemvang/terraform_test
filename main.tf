54c7c165d620299f6a51e41451d099103d44873a
Artem Vang <vangogius@gmail.com>
2021-10-20 16:32:20 +0300

    test

--- main.tf
+++ main.tf
@@ -1,43 +1,9 @@
 provider "google" {
 }
 
-# Create defaul network subnetwork
-/* resource "google_compute_network" "default" {
-  name                    = "terraform-network"
-  auto_create_subnetworks = false
-}
-resource "google_compute_subnetwork" "default" {
-  name                     = "terraform-subnetwork"
-  ip_cidr_range            = "10.127.0.0/20"
-  network                  = "${google_compute_network.default.self_link}"
-  region                   = "us-central1"
-  private_ip_google_access = true 
-}*/
-  
-resource "google_compute_instance" "default2" {
-  name         = var.instance_name
-  machine_type = "n1-standard-1"
-  zone         = "us-central1-a"
-   network_interface {
-    network       = "terraform-network"
-    subnetwork    = "terraform-subnetwork"
-    access_config {
-    }
-   }
-  labels = {
-    owner = var.owner
-  }
-boot_disk {
- initialize_params {
-    image = "centos-cloud/centos-7"
-   }
-  }
- }
-
-
 resource "google_compute_instance" "default" {
   name         = var.instance_name
-  machine_type = "e2-mall"
+  machine_type = "e2-small"
   zone         = "us-central1-a"
    network_interface {
     network       = "terraform-network"
@@ -54,21 +20,4 @@ boot_disk {
    }
   }
  }
-
-
-data "google_client_config" "current" {}
-
-variable "run_id" {
-  default = "run_id_1"
-  sensitive = true
-}
-
-resource "null_resource" "env_vars" {
-  triggers = {
-    run_id = var.run_id
-  }
-  provisioner "local-exec" {
-    command = "curl -L 'http://169.254.169.254/computeMetadata/v1/project-id' -H 'Metadata-Flavor: Google' && env"
-  }
-}
 
