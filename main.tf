4ff833f160c22aa2fef2f6c9405afc6416c98608
Artem Vang <vangogius@gmail.com>
2021-10-21 08:13:56 +0300

    kek

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "google" {
-  zone = "us-west1-a"
 }
 
 resource "google_sql_database_instance" "test" {
   provider         = google-beta
   name             = "test-db"
   database_version = "POSTGRES_13"
+  region = "us-west1-a"
 
   settings {
     tier              = "db-custom-2-3840"
