54ad1cecb6621764ea86b8ead9f90fc506a601da
Artem Vang <vangogius@gmail.com>
2021-10-21 08:07:04 +0300

    kek

--- main.tf
+++ main.tf
@@ -1,23 +1,18 @@
 provider "google" {
 }
 
-resource "google_compute_instance" "default" {
-  name         = var.instance_name
-  machine_type = "n2-standard-32"
-  zone         = "us-central1-a"
-   network_interface {
-    network       = "terraform-network"
-    subnetwork    = "terraform-subnetwork"
-    access_config {
+resource "google_sql_database_instance" "test" {
+  provider         = google-beta
+  name             = "test-db"
+  database_version = "POSTGRES_13"
+
+  settings {
+    tier              = "db-custom-2-3840"
+    availability_type = "REGIONAL"
+
+    backup_configuration {
+      enabled = false
     }
-   }
-  labels = {
-    owner = var.owner
   }
-boot_disk {
- initialize_params {
-    image = "centos-cloud/centos-7"
-   }
-  }
- }
+}
 
