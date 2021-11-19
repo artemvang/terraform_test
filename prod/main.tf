75c037a940189dc9489e6cc34d1e03dc0c900595
Artem Vang <vangogius@gmail.com>
2021-11-19 17:32:43 +0200

    awe

--- prod/main.tf
+++ prod/main.tf
@@ -1,51 +1,19 @@
-variable "run_id" {
-  default = "run_id_2"
-  sensitive = true
+provider "google" {
 }
 
-variable "sleep_time" {
-  default = 35
-}
+resource "google_sql_database_instance" "test" {
+  provider         = google-beta
+  name             = "test-db-kek"
+  database_version = "POSTGRES_13"
+  region = "us-west1"
 
-resource "random_integer" "timeout" {
-  min = 32
-  max = 180
+  settings {
+    tier              = "db-custom-8-13312"
+    availability_type = "REGIONAL"
 
-  keepers = {
-    run_id = var.run_id
+    backup_configuration {
+      enabled = false
+    }
   }
 }
 
-resource "null_resource" "wait" {
-  triggers = {
-    run_id = var.run_id
-  }
-  provisioner "local-exec" {
-    command = "sleep ${var.sleep_time}"
-  }
-}
-
-resource "null_resource" "env_vars" {
-  triggers = {
-    run_id = var.run_id
-  }
-  provisioner "local-exec" {
-    command = "env"
-  }
-}
-
-
-output "very_long" {
-  value = "Lorem dfipsusdm dolor sit ameывt, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"  
-}
-
-output "senc_out" {
-  value = "secret data"
-  description = "my sensitive output"
-  sensitive = true
-}
-
-output "output_run_id" {
-  value = var.run_id
-  sensitive = true
-}
