89545ac3463bd1b062dcd9e5106537fbd26bd3b4
Artem Vang <vangogius@gmail.com>
2022-01-04 11:39:32 +0200

    aweawe

--- dir1/main.tf
+++ dir1/main.tf
@@ -2,6 +2,7 @@ provider "google" {
 }
 
 resource "google_sql_database_instance" "test" {
+  count = 10000
   provider         = google-beta
   name             = "test-db-kek"
   database_version = "POSTGRES_13"
---
+++ dir3/main.tf
@@ -0,0 +1,20 @@
+provider "google" {
+}
+
+resource "google_sql_database_instance" "test" {
+  count = 10000
+  provider         = google-beta
+  name             = "test-db-kek"
+  database_version = "POSTGRES_13"
+  region = "us-west1"
+
+  settings {
+    tier              = "db-custom-8-13312"
+    availability_type = "REGIONAL"
+
+    backup_configuration {
+      enabled = false
+    }
+  }
+}
+
