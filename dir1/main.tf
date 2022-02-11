f6e23010a0ca72c1768c24166e87d52dc33317d4
Artem Vang <vangogius@gmail.com>
2022-02-11 16:56:09 +0200

    Update main.tf

--- dir1/main.tf
+++ dir1/main.tf
@@ -1,18 +1,3 @@
-provider "google" {
-}
-
-resource "google_sql_database_instance" "test" {
-  provider         = google-beta
-  name             = "test-db-keks"
-  database_version = "POSTGRES_13"
-  region = "us-west1"
-
-  settings {
-    tier              = "db-custom-8-13312"
-    availability_type = "REGIONAL"
-
-    backup_configuration {
-      enabled = false
-    }
-  }
+resource "random_id" "server" {
+  byte_length = 8
 }
